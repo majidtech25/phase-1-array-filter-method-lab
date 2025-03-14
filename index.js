374a99a73a3ce2425d123f318f35642e6996ea4b
majidtech25 <abdulshide87@gmail.com>
2025-03-14 07:23:03 +0300

    Assignment-filter-method

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solution here
+let drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+function findMatching(drivers, name) {
+  return drivers.filter((driver) => driver.toUpperCase() == name.toUpperCase());
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter((driver) => driver[0] === name[0]);
+}
+
+// function fuzzyMatch(drivers, name) {
+//   let LetterOne = drivers.filter((driver) => driver.firstletter(name));
+//   return LetterOne;
+// }
+
+let driversnext = ["Sammy", "Sarah", "Sally", "Tommy", "Jimmy"];
+
+console.log(fuzzyMatch(drivers, "Sa")); // ["Sammy", "Sarah", "Sally"]
+console.log(fuzzyMatch(drivers, [5], "y")); // [] (no names start with "y")
+console.log(fuzzyMatch(drivers, [3, 4], "mm"));
+
+// console.log(fuzzyMatch(drivers, "Sa"));
+const driversandhome = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+function matchName(drivers, hometown) {
+  return drivers.filter((driver) => {
+    return driver.name === hometown;
+  });
+}
